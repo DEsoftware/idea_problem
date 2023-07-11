8247bec0294f6b754f5ded9fea30cd7b3477a8b9
Hierlmeier, Richard <richard.hierlmeier@de-gmbh.com>
2023-07-11 13:09:05 +0200

    Problem reproduced

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # idea_problem
+
 Sample repository to reproduce a build problem in Intellij Idea
+
+To reproduces the problem open the project in Intellij Idea.
+* Run mvn clean
+* Open the project in Intellij Idea.
+* Build the project with Intellij Idea.
+
---
+++ generate/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+
+import com.desoft._0000_999.servicesFeatureTest.xmlA.types.TestXmlServiceAConfig;
+
+public class Main {
+    public static void main(String[] args) {
+
+        TestXmlServiceAConfig config = new TestXmlServiceAConfig();
+
+
+    }
+}
